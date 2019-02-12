7071a6e82184efc8505509f85f59274ef52d33fd
Hugo Morais <hugodanielmorais@gmail.com>
2019-02-12 18:18:38 +0000

    signin, singout, token, auth

--- learning/backend/config/routes.js
+++ learning/backend/config/routes.js
@@ -1,4 +1,9 @@
 module.exports = app => {
+    app.post('/signup', app.api.user.save)
+    app.post('/signin', app.api.auth.signin)
+    app.post('/validateToken', app.api.auth.validateToken)
+
+
     app.route('/users')
         .post(app.api.user.save)
         .get(app.api.user.get)
@@ -27,4 +32,7 @@ module.exports = app => {
         .get(app.api.article.getById)
         .put(app.api.article.save)
         .delete(app.api.article.remove)
+
+    app.route('/categories/:id/articles')
+        .get(app.api.article.getByCategory)
 }
