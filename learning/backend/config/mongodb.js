d01b3c6212f0a784e839c0ddbb62651369ea0462
Hugo Morais <hugodanielmorais@gmail.com>
2019-02-12 19:44:25 +0000

    stat, mongodb

---
+++ learning/backend/config/mongodb.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/learning_stats', { useNewUrlParser: true })
+    .catch(e => {
+        const msg = 'ERRO! Não foi possivel conectar com o MongoDB!'
+        console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m')
+    })
