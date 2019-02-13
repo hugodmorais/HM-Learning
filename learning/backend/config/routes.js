3787ea8686b9811e0b8f88fb98737bf745a619d8
Hugo Morais <hugodanielmorais@gmail.com>
2019-02-13 13:19:52 +0000

    validations, finish backend

--- learning/backend/config/routes.js
+++ learning/backend/config/routes.js
@@ -12,9 +12,10 @@ module.exports = app => {
         .get(admin(app.api.user.get))
 
     app.route('/users/:id')
-    .all(app.config.passport.authenticate())
+        .all(app.config.passport.authenticate())
         .put(admin(app.api.user.save))
         .get(admin(app.api.user.getById))
+        .delete(admin(app.api.user.remove))
 
     app.route('/categories')
         .all(app.config.passport.authenticate())
