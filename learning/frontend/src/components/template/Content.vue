e56d2e5b4c420c7fc3fbbc4b87c963a411abec58
Hugo Morais <hugodanielmorais@gmail.com>
2019-02-14 13:03:49 +0000

    stat values

--- learning/frontend/src/components/template/Content.vue
+++ learning/frontend/src/components/template/Content.vue
@@ -15,5 +15,6 @@ export default {
         grid-area: content;
         background-color: #fff;
         padding: 20px;
+        box-shadow: 0 1px 5px rgba(0, 0, 0, 0.15);
     }
 </style>
--- learning/frontend/src/main.js
+++ learning/frontend/src/main.js
@@ -9,6 +9,8 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Ikh1Z28gTW9yYWlzIiwiZW1haWwiOiJodWdvQGdtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE1NTAxNDg1NjUsImV4cCI6MTU1MDQwNzc2NX0.ZV8zKH8YRn8rKFcT299Xtd2aq4gRZq-YTOpSG0ETK2U'
+
 new Vue({
   store,
   router,
